d5f7e3391ca1ddb66e68142fa3286d6592974e78
Mohasin Shaikh <mohasinshaikh0207@gmail.com>
2026-08-18 17:01:29 +0530

    Variables

---
+++ Domain/day-5/index.js
@@ -0,0 +1,46 @@
+// let m="mohasin";
+// console.log(m);
+// let int =12;
+// let num=15.5;
+// let s =true;
+// console.log(int,s,num);
+// var a =20;
+// var a=223;
+// console.log(a);
+// const a1=20;
+// //a1=39;
+// console.log(a1);
+// let m="mohasin shaikh"
+// console.log(m);
+// let b ;
+// console.log(b);
+// let c =null;
+// console.log(c);
+// let aa=1234567891;
+// console.log(aa);
+// let arr=[10,20,49,55,"rohit"];
+// arr[0]=20;
+// arr.push(11);
+// console.log(arr);
+
+// let a={
+//     name:"rohiy",
+//     age :21,
+//     accoout :1231,
+//     category: "gen"
+    
+// }
+// console.log(a);
+function run(fact)
+{
+   
+    for(let i=1;i<=fact;i++){
+        console.log(i);
+    }
+}
+let a =function pay()
+{
+    console.log(12);
+
+}
+console.log(a);
