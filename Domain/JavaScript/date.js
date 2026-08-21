95923daf80e08d4f1a6e232b5811b01f822673da
Mohasin Shaikh <mohasinshaikh0207@gmail.com>
2026-08-21 21:06:32 +0530

    basics

---
+++ Domain/JavaScript/date.js
@@ -0,0 +1,6 @@
+let mydate=new Date()
+console.log(mydate.toString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toUTCString())
+console.log(mydate.toISOString())
+console.log(mydate.toLocaleDateString())
---
+++ Domain/JavaScript/number_and_math.js
@@ -0,0 +1,35 @@
+// const score=400;
+// console.log(score);
+// const balance =new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(3));
+// const otherNumber=1123.8966
+
+// console.log(otherNumber.toPrecision(2));
+
+// const hundeds=1000000;
+// console.log(hundeds.toLocaleString())
+
+// ++++++++++++ Math +++++++++++//
+console.log(Math)
+
+console.log(Math.abs(-4));
+console.log(Math.abs(5));
+
+
+console.log(Math.round(5,4))
+
+console.log(Math.ceil((3.0)));
+console.log(Math.floor(4.9));
+
+
+console.log((Math.random()*10 )+ 1);
+
+const min=10;
+const max=20;
+
+Math.random()
+console.log(Math.floor((Math.random() * 10) +1))
+
