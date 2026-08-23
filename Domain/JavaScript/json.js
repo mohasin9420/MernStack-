a9554d58b51bfc71059bc5b911a5d6d02e019edb
Mohasin Shaikh <mohasinshaikh0207@gmail.com>
2026-08-23 17:52:21 +0530

    Add JavaScript Scoops,json

---
+++ Domain/JavaScript/json.js
@@ -0,0 +1,15 @@
+const course=
+{
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+
+}
+const {courseInstructor:ms} = course
+console.log(ms)
+
+// {
+//     name:"mohasin";
+//     price:"999",
+//     courseInstructor:"hitesh"
+// }
