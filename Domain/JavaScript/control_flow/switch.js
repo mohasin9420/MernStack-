1ee5fd12335615a89bc8d1e6c4803cbb9ecdb30c
Mohasin Shaikh <mohasinshaikh0207@gmail.com>
2026-08-26 17:55:51 +0530

    control flow of js

---
+++ Domain/JavaScript/control_flow/switch.js
@@ -0,0 +1,28 @@
+let day=9;
+switch(day)
+{
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("Tuesday")
+        break;
+    case 3:
+        console.log("Wensday")
+        break;
+    case 4:
+        console.log("thusday")
+        break;
+    case 5:
+        console.log("Friday")
+        break;
+    case 6:
+        console.log("saturday")
+        break;
+    case 7:
+        console.log("Sunday")
+        break;
+    default:
+        console.log("input wrong")
+        break;
+}
